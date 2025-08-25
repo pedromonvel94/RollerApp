744c56bc1585b835a2dd2aa0ab7b18a6347c2834
pedromonvel94 <pedro.montoya@est.iudigital.edu.co>
2025-08-25 15:21:59 -0500

    MVP 1 Caso de estudio Roller Spreed

---
+++ README.md
@@ -0,0 +1,6 @@
+# RollerApp
+Assignment for the MVC Frameworks subject, where we will focus on developing an app using the Java framework called Spring.
+
+### Comando para correr la app de springboot
+
+./mvnw spring-boot:run
---
+++ src/main/java/com/rollerspeed/rollerspeed/HomeController.java
@@ -0,0 +1,39 @@
+package com.rollerspeed.rollerspeed;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+
+    @GetMapping("/index")
+    public String index() {
+        return "pages/index"; // busca index.html en templates/pages/index.html
+    }
+
+     @GetMapping("/mision")
+    public String mision() {
+        return "pages/mision"; // busca templates/pages/mision.html
+    }
+
+    @GetMapping("/vision")
+    public String vision() {
+        return "pages/vision"; // busca templates/pages/vision.html
+    }
+
+    @GetMapping("/valores")
+    public String valores() {
+        return "pages/valores";
+    }
+
+    @GetMapping("/servicios")
+    public String servicios() {
+        return "pages/servicios";
+    }
+
+    @GetMapping("/eventos")
+    public String eventos() {
+        return "pages/eventos";
+    }
+}
